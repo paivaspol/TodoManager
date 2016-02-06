8c82a4453e341e0f080dd5dc6ceb96f62e96888f
Vaspol Ruamviboonsuk <vaspol@umich.edu>
2016-02-06 09:05:05 -0500

    Added additional endpoints and update the requirements file.

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,14 @@
 from flask import Flask, request
+from flask.ext.cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello():
     return 'Hello World!'
 
-@app.route('/add_todo')
+@app.route('/add_todo', methods = [ 'POST' ])
 def add_todo():
     print request.method
     return 'yea'
